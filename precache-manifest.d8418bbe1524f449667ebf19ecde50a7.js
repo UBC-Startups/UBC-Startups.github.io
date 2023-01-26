deb2e28734d8c9ebc2b9e1ae7a11c564586ca963
DonaldKLee <Donald3010@outlook.com>
2023-01-26 15:16:22 -0800

    Updates

---
+++ precache-manifest.d8418bbe1524f449667ebf19ecde50a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "712306cc6a9cce07ade1",
+    "url": "/static/js/main.712306cc.chunk.js"
+  },
+  {
+    "revision": "cf042e081f8a87233510",
+    "url": "/static/js/1.cf042e08.chunk.js"
+  },
+  {
+    "revision": "25f3cc48fe7d88c000bf",
+    "url": "/static/js/2.25f3cc48.chunk.js"
+  },
+  {
+    "revision": "69133d183c995e1d60fc",
+    "url": "/static/js/runtime~main.69133d18.js"
+  },
+  {
+    "revision": "3f652b65988c34999d891c6d5dfceca4",
+    "url": "/static/media/Logo.3f652b65.svg"
+  },
+  {
+    "revision": "c62cd2d5cf2f11729f0add4f22c0da56",
+    "url": "/static/media/HeroShapeDesign.c62cd2d5.svg"
+  },
+  {
+    "revision": "e607449565d645b6479b45c9c5a2922e",
+    "url": "/index.html"
+  }
+];

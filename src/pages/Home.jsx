e683f55baad9717ff393d68787cb2570c7769eb5
Ayush Patel <104913698+Ayush200423@users.noreply.github.com>
2024-09-18 17:39:43 -0700

    Founders Spotlight

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,27 +10,28 @@ import AboutUs from "../sections/aboutUs";
 import OurValues from "../sections/ourValues";
 import MeetOurTeam from "../sections/meetOurTeam";
 import Footer from "../sections/footer";
-import Soar from "../sections/soar";
+// import Soar from "../sections/soar";
+import FoundersSpotlight from "../sections/foundersSpotlight";
 
 const HomePage = () => {
-    return (
-        <>
-          <Head title="UBC Startups"/>
-          <NavigationBar />
-          <Hero 
-            heading="UBC Startups"
-            description="Building a startup ecosystem on campus"
-          />
-          <Soar />
-          <WhatWeOffer />
-          <OurEvents />
-          <AboutUs />
-          <OurValues />
-          <MeetOurTeam />
-          {/* <Resources /> */}
-          <Footer />
-        </>
-    )
-}
+  return (
+    <>
+      <Head title="UBC Startups" />
+      <NavigationBar />
+      <Hero
+        heading="UBC Startups"
+        description="Building a startup ecosystem on campus"
+      />
+      <FoundersSpotlight />
+      <WhatWeOffer />
+      <OurEvents />
+      <AboutUs />
+      <OurValues />
+      <MeetOurTeam />
+      {/* <Resources /> */}
+      <Footer />
+    </>
+  );
+};
 
 export default HomePage;

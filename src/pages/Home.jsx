fbb187519913b9e0fcc720a2cb9eeab1daf1ca79
Ayush Patel <104913698+Ayush200423@users.noreply.github.com>
2024-10-20 19:29:24 -0700

    added form

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,6 +27,7 @@ const HomePage = () => {
         punchline="IDCreate will be our 2nd event of the year, where future entrepreneurs have the opportunity to network and connect with entrepreneurs from and outside of UBC."
         description="We aim to have a diverse panel with entrepreneurs from various faculities. Students will have time to ask panelists questions, followed by a group activity to ideate a product and pitch it to the panel. At the end, we will open up the floor for an open networking session!"
         extraDetails="This event is designed in such a way that students need not have a business background to attend, they all must have one thing in common; the love for entrepreneurship!"
+        registrationLink="https://forms.gle/vBmDxY64dyYPuPxz7"
         date="November 4th, 2024 (5:30pm to 8:30pm)"
       />
       <WhatWeOffer />
